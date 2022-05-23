2b0d2929a9577ee9edee6b0e2ac8048010fc11b6
LouisMazel <mazuel.loic@gmail.com>
2022-05-23 23:16:44 +0200

    wip

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,11 +1,13 @@
 import { defineUserConfig, SiteData, defaultTheme, viteBundler } from 'vuepress'
 import { path } from '@vuepress/utils'
+
 import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
 import { searchPlugin } from '@vuepress/plugin-search'
-import { sidebar, navbar } from './configs'
 import { sitemapPlugin } from "vuepress-plugin-sitemap2"
 import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
 import { mediumZoomPlugin } from '@vuepress/plugin-medium-zoom'
+
+import { sidebar, navbar, head } from './configs'
 
 const getBaseUrl = (): SiteData['base'] => {
   return process.env.NODE_ENV === 'production' ? `/vuepress-test/` : '/'
@@ -19,87 +21,7 @@ export default defineUserConfig({
   clientConfigFile: path.resolve(__dirname, 'client.ts'),
   base: getBaseUrl(),
 
-  head: [
-    [
-      'link',
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '16x16',
-        href: '/img/icons/favicon-16x16.png',
-      },
-    ],
-    [
-      'link',
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '32x32',
-        href: '/img/icons/favicon-32x32.png',
-      },
-    ],
-    ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
-    ['meta', { name: 'robots', content: 'index, follow' }],
-    ['meta', { name: 'application-name', content: 'Maz UI' }],
-    ['meta', { name: 'apple-mobile-web-app-title', content: 'Maz UI' }],
-    [
-      'meta',
-      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
-    ],
-    [
-      'meta',
-      { name: 'twitter:creator', content: '@mazeel' },
-    ],
-    [
-      'meta',
-      { name: 'twitter:card', content: 'summary_large_image' },
-    ],
-    [
-      'meta',
-      { name: 'twitter:site', content: '@maz__ui' },
-    ],
-    [
-      'meta',
-      { property: 'og:title', content: 'Maz-UI - Vue.JS & Nuxt.JS library' },
-    ],
-    [
-      'meta',
-      { property: 'og:description', content: 'Library of standalone components and tools for Vue & Nuxt (v3.x)' },
-    ],
-    [
-      'meta',
-      { property: 'og:type', content: 'website' },
-    ],
-    [
-      'meta',
-      { name: 'twitter:title', content: 'Maz-UI - Vue.JS & Nuxt.JS library' },
-    ],
-    [
-      'meta',
-      { name: 'twitter:description', content: 'Library of standalone components and tools for Vue & Nuxt (v3.x)' },
-    ],
-    ['meta', { property: 'og:image', content: '/img/maz-ui-preview.jpg' }],
-    ['meta', { name: 'twitter:image', content: '/img/maz-ui-preview.jpg' }],
-    [
-      'link',
-      { rel: 'apple-touch-icon', href: '/img/icons/apple-touch-icon.png' },
-    ],
-    [
-      'link',
-      { property: 'og:site_name', content: 'Maz UI' },
-    ],
-    [
-      'link',
-      {
-        rel: 'mask-icon',
-        href: '/img/icons/safari-pinned-tab.svg',
-        color: '#3eaf7c',
-      },
-    ],
-    ['meta', { name: 'msapplication-TileColor', content: '#2d89ef' }],
-    ['meta', { name: 'theme-color', content: '#ffffff' }],
-    ['meta', { name: 'msapplication-TileImage', content: '/img/maz-ui-preview.jpg' }],
-  ],
+  head,
 
   // theme and its config
   theme: defaultTheme({
